282463c20ebd71840ffaff4e6afdcbeec8d8ca2f
leon-zhang <leon.zhang@g-pc-3559.dev.gree.jp>
2013-07-24 18:32:33 +0900

    daliy commit

--- Login.php
+++ Login.php
@@ -1,10 +1,57 @@
 <?php
 
- set_include_path("../");
+ //set_include_path("../");
+
+ class DBMgr
+ {
+  public static function Inst()
+  {
+   static $inst = null;
+   if($inst === null) {
+    $inst = new DBMgr();
+   }
+   return $inst;
+  }
+
+  private var $mysqli = null;
+
+  public function __construct()
+  {
+   $mysqli = null;
+  }
+
+  public function connect($server,$user,$pwd,$db)
+  {
+   bool conn=false;
+
+   $mysqli = new mysqli($server,$user,$pwd,$db);
+
+   return conn;
+  }
+  public function disconnect()
+  {
+   $mysqli->close();
+   unset($mysqli);
+   $mysqli=null;
+  }
+  public function ExecQuery($sql)
+  {
+   $mysqli->query($mysqli->real_escape_string($sql));
+  }
+　public function isConnected()
+  {
+   return ($mysqli!=null);
+  }
+ }
 
  class LoginMgr
  {
   public static $SESSION_NAME = "SESSION_LoginMgr";
+  private static $_self = null;
+
+  var $sql_queries = array(
+   'find_user'     => 'SELECT in %s where %s="%s"',
+  );
 
   private var $user_name;
   
@@ -22,19 +69,20 @@
 
   public function logout()
   {
-   $user_name="";
+   unset($_SESSION[self::$SESSION_NAME]);
   }
 
   public function isLoggedin()
   {
-   return (strlen($user_name)>0);
+   return isset($_SESSION[self::$SESSION_NAME]);
   }
 
   public static function Inst()
   {
-   if(isset($_SESSION[self::$SESSION_NAME]))
-    return $_SESSION[self::$SESSION_NAME];
-
-   return null;
+   static $inst = null;
+   if($inst === null) {
+    $inst = new LoginMgr();
+   }
+   return $inst;
   }
- };
+ }
