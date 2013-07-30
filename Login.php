17efd5301f9f3b70f921996b7fe69bcbbba231e8
leon-zhang <leon.zhang@g-pc-3559.dev.gree.jp>
2013-07-30 19:18:28 +0900

    daliy

--- Login.php
+++ Login.php
@@ -13,34 +13,44 @@
    return $inst;
   }
 
-  private var $mysqli = null;
+  private var $mysqli_conn;
 
   public function __construct()
   {
-   $mysqli = null;
+   $mysqli_conn = null;
   }
 
   public function connect($server,$user,$pwd,$db)
   {
-   bool conn=false;
+   bool $conn=false;
 
-   $mysqli = new mysqli($server,$user,$pwd,$db);
+   $mysqli_conn = new mysqli($server,$user,$pwd,$db);
 
-   return conn;
+   return $conn;
   }
   public function disconnect()
   {
-   $mysqli->close();
-   unset($mysqli);
-   $mysqli=null;
+   $mysqli_conn->close();
+   unset($mysqli_conn);
+   $mysqli_conn=null;
   }
   public function ExecQuery($sql)
   {
-   $mysqli->query($mysqli->real_escape_string($sql));
+   $mysqli_conn->query($mysqli->real_escape_string($sql));
+  }
+
+  public function ExecQuery($sql,string $types, mixed &$var1 [, mixed &$... ])
+  {
+   $stmt = $mysqli_conn->prepare($sql);
+   $stmt->bind_param($types, $var1);
+
+   $stmt->execute();
+
+   return $stmt->get_result();
   }
 　public function isConnected()
   {
-   return ($mysqli!=null);
+   return ($mysqli_conn!=null);
   }
  }
 
@@ -49,8 +59,16 @@
   public static $SESSION_NAME = "SESSION_LoginMgr";
   private static $_self = null;
 
+  var $column_names = array(
+   "__DB_USERTABLE__" => "User",
+  );
+  var $column_names = array(
+
+   "__COL_USERNAME__" => "username",
+   "__COL_PWD__" => "pwd",
+  );
   var $sql_queries = array(
-   'find_user'     => 'SELECT in %s where %s="%s"',
+   'find_user_by_pwd'     => 'SELECT * FROM __DB_USERTABLE__ WHREE __COL_USERNAME__=? AND __COL_PWD__=?',
   );
 
   private var $user_name;
